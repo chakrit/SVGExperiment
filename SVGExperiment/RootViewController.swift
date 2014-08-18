47632b181ed4520b5a81c3986d4b367786fec411
Chakrit Wichian <service@chakrit.net>
2014-08-18 13:50:19 +0700

    Switch to layered image view + scaling fixes.

--- SVGExperiment/RootViewController.swift
+++ SVGExperiment/RootViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class RootViewController: UIViewController {
-    typealias ImageView = SVGKFastImageView
+    typealias ImageView = SVGKLayeredImageView
 
     override func loadView() {
         var screenSize = UIScreen.mainScreen().bounds.size
@@ -10,26 +10,24 @@ class RootViewController: UIViewController {
         }
 
         let frame = CGRect(origin: CGPointZero, size: screenSize)
-
         let v = UIView(frame: frame)
         v.autoresizingMask = .FlexibleWidth | .FlexibleHeight
         v.backgroundColor = .whiteColor()
 
-        let contentSize = CGSize(width: 1024, height: 630)
-        let svgFrame = CGRect(origin: CGPointZero, size: contentSize)
-
+        let image = SVGKImage(named: "twinkle.svg")
+        
+        let svgFrame = CGRect(origin: CGPointZero, size: image.size)
         let svg = ImageView(frame: svgFrame)
         svg.setTranslatesAutoresizingMaskIntoConstraints(false)
         svg.backgroundColor = .whiteColor()
         svg.contentMode = .ScaleAspectFit
-        svg.image = SVGKImage(named: "Twinkle.svg")
-
+        svg.image = image
         v.addSubview(svg)
+
         v.addConstraint(NSLayoutConstraint(horizontalAlignItem: svg, withItem: v))
         v.addConstraint(NSLayoutConstraint(verticalAlignItem: svg, withItem: v))
-        v.addConstraint(NSLayoutConstraint(item: svg, width: contentSize.width))
-        v.addConstraint(NSLayoutConstraint(item: svg, height: contentSize.height))
-        
+        v.addConstraint(NSLayoutConstraint(item: svg, width: image.size.width))
+        v.addConstraint(NSLayoutConstraint(item: svg, height: image.size.height))
         view = v
     }
 }
